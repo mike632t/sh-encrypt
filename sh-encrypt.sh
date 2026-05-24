a8f8f74383f6283424f99eec0602bf5ecad203c7
mike632t <1799044+mike632t@users.noreply.github.com>
2026-05-24 01:03:31 +0100

    Added hint to 'Bad decrypt' error message

--- sh-encrypt.sh
+++ sh-encrypt.sh
@@ -50,6 +50,7 @@
 #                      prompted to confirm - MT
 #                    - File size and temporary filename generation modified
 #                      to make them more portable - MT
+#  24 May 26         - Added hint to 'Bad decrypt' error message - MT
 # 
 #
 #  ToDo              - 
@@ -278,7 +279,7 @@ if [ $_status -eq 0 ]; then  # Check there were no errors on the command line.
                _scratch=$(mktemp "$_tmp/tmpfile.XXXXXX")  # Create a temporary file.
                if [ "$_status" -eq 0 ]; then
                   (cat "$_filename" 2>&1 >&3 3>&- | sed "1s|^cat: |$0: |" >&2 3>&-) 3>&1 | \
-                  (openssl enc $_options -k "$_password" $_mode 2>&1 >&3 3>&- | sed "1s|^|$0: |" | sed -n 1,2p | sed "s|error reading input file|& (is it plain text)|" >&2 3>&-) 3>&1 | cat > "$_scratch"  # Encrypt or decrypt file rewriting an error messages.
+                  (openssl enc $_options -k "$_password" $_mode 2>&1 >&3 3>&- | sed "1s|^|$0: |" | sed -n 1,2p | sed "s|error reading input file|& (is it plain text)|" | sed "s|bad decrypt|& (try legacy options)|" >&2 3>&-) 3>&1 | cat > "$_scratch"  # Encrypt or decrypt file rewriting an error messages.
                   _status=$?
                   if [ $_status -eq 0 ]; then
                      if [ $_force -eq 1 ] || confirm "Overwrite existing file(s)"; then  # Confirm deletion of original file.
@@ -305,7 +306,7 @@ if [ $_status -eq 0 ]; then  # Check there were no errors on the command line.
                fi
             else
                (cat "$_filename" 2>&1 >&3 3>&- | sed "1s|^cat: |$0: |" >&2 3>&-) 3>&1 | \
-               (openssl enc $_options -k "$_password" $_mode 2>&1 >&3 3>&- | sed "1s|^|$0: |" | sed -n 1,2p | sed "s|error reading input file|& (is it plain text)|" >&2 3>&-) 3>&1 | cat  # Encrypt or decrypt file rewriting an error messages.
+               (openssl enc $_options -k "$_password" $_mode 2>&1 >&3 3>&- | sed "1s|^|$0: |" | sed -n 1,2p | sed "s|error reading input file|& (is it plain text)|" | sed "s|bad decrypt|& (try legacy options)|" >&2 3>&-) 3>&1 | cat  # Encrypt or decrypt file rewriting an error messages.
                _status=$?
             fi
          else
